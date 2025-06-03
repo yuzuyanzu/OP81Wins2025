f9e33b0a0b92ab3a8e6c63f0b9ec75587be41911
Myan <129897782+yuzuyanzu@users.noreply.github.com>
2025-06-03 18:46:24 -0400

    Create OP81Wins.py

---
+++ OP81Wins.py
@@ -0,0 +1,9 @@
+previous_wins_for_2025 = 4
+
+print("Oscar Piastri wins for 2025")
+
+added_number_of_wins_for_2025 = 1
+
+season_wins_for_2025 = previous_wins_for_2025 + added_number_of_wins_for_2025
+
+print("Total wins:", season_wins_for_2025)
